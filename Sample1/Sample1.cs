22ca5ce19ea27630334a97a2159e4cdb51f62fe7
jurin-n <jurin.nakano@gmail.com>
2020-07-18 10:26:38 +0900

    新規作成

---
+++ Sample1/Sample1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sample1
+{
+    class Sample1
+    {
+        static string program_ver = "sample #1";
+        static void Main() { 
+            Console.WriteLine(program_ver);
+            Console.Write("お名前は？＞＞");
+            string name = System.Console.ReadLine();
+            Console.WriteLine($"ようこそ、{name}さん！");
+        }
+    }
+}
