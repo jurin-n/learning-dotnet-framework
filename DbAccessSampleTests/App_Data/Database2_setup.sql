1a983b83ec0445e0de26d4a2d5d3cbd0269f5d2b
jurin-n <jurin.nakano@gmail.com>
2020-07-23 19:12:55 +0900

    データベースファイル追加。

---
+++ DbAccessSampleTests/App_Data/Database2_setup.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Item](
+    [ItemID] [int] NOT NULL,  
+    [Name] [nvarchar](50) NULL,  
+    [Description] [nvarchar](512) NULL,
+    [CreatedOn] [datetime] NOT NULL
+CONSTRAINT [PK_Item] PRIMARY KEY CLUSTERED   
+(  
+[ItemID] ASC  
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]) ON [PRIMARY]  
+  
+GO  
