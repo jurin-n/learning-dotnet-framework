4377df34a6efc7a0d848bbdbef123632742ecc19
jurin-n <jurin.nakano@gmail.com>
2020-07-23 17:40:31 +0900

    単体テストプロジェクト追加。

---
+++ DbAccessSample/TransactionScopeSample.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DbAccessSample
+{
+    public class TransactionScopeSample
+    {
+        public void Add() {
+            Console.WriteLine("処理開始");
+        }
+    }
+}
---
+++ DbAccessSampleTests/TransactionScopeSampleTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DbAccessSample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAccessSample.Tests
+{
+    [TestClass()]
+    public class TransactionScopeSampleTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            TransactionScopeSample t = new TransactionScopeSample();
+            t.Add();
+            Assert.Fail();
+        }
+    }
+}
