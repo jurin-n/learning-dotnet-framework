2de012a1a661c14e56351dc9ef4e5705859738e5
jurin-n <jurin.nakano@gmail.com>
2020-07-24 09:54:02 +0900

    Typo

--- DbAccessSample/DbHelper.cs
+++ DbAccessSample/DbHelper.cs
@@ -12,12 +12,12 @@ namespace DbAccessSample
         public static String getConnectionString()
         {
             //パスワード抜きのConnectionStringを取得し
-            //SqlConnectionStringBuilderオブジェクト作成。
+            //SqlConnectionStringBuilderオブジェクト作成する。
             String tempConnectionString = "Data Source=localhost; Database = master;User ID=user01;Password=; Min Pool Size=100;Max Pool Size=1000";
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(tempConnectionString);
 
             //パスワードは、暗号化した状態で格納する。
-            //ここのロジックで、暗号化したパスワードを複合化して
+            //ここのロジックで、暗号化したパスワードを復号化して
             //builder.Password にセットすることが考えられる。
             builder.Password = "user01password";
             return builder.ToString();
